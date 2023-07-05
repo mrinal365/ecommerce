b519829522952009f9b64d827d5f6447fb1d4c6f
Mrinal <mrinaltewary@gmail.com>
2023-07-06 02:11:34 +0530

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from 'styled-components';
 
+//Import Material icons
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import {Badge} from '@material-ui/core'
+
 const Container = styled.div`
     height: 60px;
 `
@@ -33,16 +37,26 @@ const Input = styled.input`
 
 `
 const Center = styled.div`
-  flex:1
+  flex:1;
+  text-align:center;
 `
 
 const Logo = styled.h1`
   font-weight:bold;
-  
+
 `
 const Right = styled.div`
-  flex:1  
+  flex:1;
+  display:flex;
+  align-items: center;
+  justify-content: flex-end;
 `
+ const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left:25px;
+ `
+
 
 function Navbar() {
   return (
@@ -52,11 +66,22 @@ function Navbar() {
           <Language>EN</Language>
           <SearchContainer>
             <Input/>
-            {/* <Search/> */}
+            <Search style={{color:'grey', fontSize:16}}/>
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>MyWhaleHub</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color='primary' >
+              <ShoppingCartOutlined/>
+            </Badge>
+            {/* <Badge/>  */}
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   )
