2b70135cbc77bb18d21660b05865da1637840577
Mrinal <mrinaltewary@gmail.com>
2023-07-09 23:47:54 +0530

    products page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,15 @@
+import { styled } from "styled-components"
+import Navbar from "../components/Navbar"
+
+const Container = styled.div`
+`
+
+function Product() {
+  return (
+    <Container>
+        <Navbar/>
+    </Container>
+  )
+}
+
+export default Product
