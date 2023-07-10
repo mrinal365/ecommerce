c9cb36324af5e6e8c127e7444a80f1c6d1485b40
Mrinal <mrinaltewary@gmail.com>
2023-07-10 17:11:54 +0530

    product page done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import ProductList from './pages/ProductList';
+import Product from './pages/Product';
 
 function App() {
   return (
     // <div style={{backgroundImage:"url('/assets/bg.png')",backgroundPosition:'center',backgroundSize:'cover',objectFit:'cover'}}>
     <div>
       {/* <Home /> */}
-      <ProductList/>
+      {/* <ProductList/> */}
+      <Product/>
     </div>
   );
 }
