e855418550c6b62061dee2d8453bd603fb0db87c
Mrinal <mrinaltewary@gmail.com>
2023-07-27 23:59:12 +0530

    api product

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,8 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { styled } from "styled-components"
 import { popularProducts } from "../utils/sliderData"
-import Product from "./Product"
+import Product from "./Product";
 
 const Container = styled.div`
     padding: 20px;
@@ -9,8 +11,33 @@ const Container = styled.div`
     justify-content:space-between;
 `
 
+function Products({cat, filters, sort}) {
 
-function Products() {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([])
+  console.log(cat, filters, sort);
+  
+  useEffect(()=>{
+      const getProduct = async()=>{
+        try{
+          const res = await axios.get(
+            cat ?`http://localhost:8001/product/products?category=${cat}`
+            : "http://localhost:8001/product/products"
+          )
+          setProducts(res.data);
+        } catch(err){
+
+        }
+      }
+      getProduct();
+  },[cat])
+
+  useEffect(()=>{
+    cat&setFilteredProducts(
+      produ
+    )
+  }, [cat,filters])
+  
   return (
     <Container>
         {popularProducts.map((item)=>(
