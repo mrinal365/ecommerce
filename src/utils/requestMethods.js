e728a9ea543921088a10260c268ce5017d285130
Mrinal <mrinaltewary@gmail.com>
2023-08-12 23:53:51 +0530

    dunamic token

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const BASE_URL = "http://localhost:8001/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YjUzY2Q2NWFmOTU2ZWM3MTBjNTVkNyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5MDg3Nzc4NiwiZXhwIjoxNjkxMTM2OTg2fQ.o4l00vWOWjokmZSslMPXf2eZWepTaRxDpiwcbWY50G8";
+const TOKEN =""
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).currentUser).accessToken;
+console.log("---", TOKEN,JSON.parse(localStorage.getItem('persist:root')).currentUser)
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
